d4519dd16c1a6764a1237488c5f7352382cc2012
Harshavardhan <hvr2026@gmail.com>
2025-10-24 09:37:24 -0700

    Add files via upload

---
+++ Agents with ADK/Travel Agent using MCP Toolbox for Databases and Agent Development Kit/hotel-agent-app/agent.py
@@ -0,0 +1,24 @@
+from google.adk.agents import Agent
+from toolbox_core import ToolboxSyncClient
+
+#toolbox = ToolboxSyncClient("http://127.0.0.1:5000")
+
+toolbox = ToolboxSyncClient("https://toolbox-531524326971.us-central1.run.app")
+
+# Load single tool
+# tools = toolbox.load_tool('search-hotels-by-location')
+
+# Load all the tools
+tools = toolbox.load_toolset('my_first_toolset')
+
+root_agent = Agent(
+    name="hotel_agent",
+    model="gemini-2.5-flash",
+    description=(
+        "Agent to answer questions about hotels in a city or hotels by name."
+    ),
+    instruction=(
+        "You are a helpful agent who can answer user questions about the hotels in a specific city or hotels by name. Use the tools to answer the question"
+    ),
+    tools=tools,
+)
